ec79becd890c71c62e4be4ad7d6387dced76d71b
Ole Begemann <ole@oleb.net>
2022-11-26 16:19:29 +0100

    Use sections in sidebar

--- OnAppearInScrollView/RootView.swift
+++ OnAppearInScrollView/RootView.swift
@@ -1,47 +1,92 @@
 import SwiftUI
 
-enum CaseStudy: String, CaseIterable, Identifiable {
-    case scrollView = "ScrollView with static content"
-    case scrollViewVStackForEach = "ScrollView > VStack > ForEach"
-    case list = "List with dynamic content"
-
-    var id: Self {
-        self
-    }
-
-    var label: String {
-        rawValue
-    }
+struct Category: Identifiable {
+    var id: String
+    var label: String
+    var elements: [CaseStudy]
 }
 
+struct CaseStudy: Identifiable, Hashable {
+    var id: String
+    var label: String
+    var description: String?
+}
+
+extension CaseStudy {
+    static let scrollView: Self = .init(
+        id: "scrollView",
+        label: "ScrollView with static content"
+    )
+    static let scrollViewVStackForEach: Self = .init(
+        id: "scrollViewVStackForEach",
+        label: "ScrollView > VStack > ForEach",
+        description: "A VStack with dynamic content, embedded in a ScrollView"
+    )
+    static let list: Self = .init(
+        id: "list",
+        label: "List with dynamic content"
+    )
+}
+
+let categories: [Category] = [
+    Category(
+        id: "scrollview",
+        label: "ScrollView",
+        elements: [
+            .scrollView,
+            .scrollViewVStackForEach,
+        ]
+    ),
+    Category(
+        id: "list",
+        label: "List",
+        elements: [
+            .list
+        ]
+    ),
+]
+
 struct RootView: View {
-    @State private var selection: CaseStudy? = nil
     @State private var columnVisibility: NavigationSplitViewVisibility = .doubleColumn
 
     var body: some View {
         NavigationSplitView(columnVisibility: $columnVisibility) {
-            Sidebar(selection: $selection)
+            Sidebar()
         } detail: {
-            if let selection {
-                Primary(caseStudy: selection)
-            } else {
-                Text("Select an item in the sidebar.")
-            }
+            Text("Select an item in the sidebar.")
         }
     }
 }
 
 struct Sidebar: View {
-    @Binding var selection: CaseStudy?
+    @State private var selection: CaseStudy? = nil
 
     var body: some View {
-        List(CaseStudy.allCases, selection: $selection) { caseStudy in
-            NavigationLink(value: caseStudy) {
-                Text(caseStudy.label)
+        List {
+            ForEach(categories) { section in
+                Section {
+                    ForEach(section.elements) { caseStudy in
+                        NavigationLink(value: caseStudy) {
+                            VStack(spacing: 8) {
+                                Text(caseStudy.label)
+                                if let description = caseStudy.description {
+                                    Text(description)
+                                        .font(.callout)
+                                        .foregroundStyle(.secondary)
+                                }
+                            }
+                        }
+                    }
+                } header: {
+                    Text(section.label)
+                }
             }
         }
         .navigationTitle("When is onAppear called?")
         .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(for: CaseStudy.self) { caseStudy in
+            Primary(caseStudy: caseStudy)
+        }
     }
 }
 
@@ -56,6 +101,8 @@ struct Primary: View {
             CaseStudyScrollViewVStackForEach()
         case .list:
             CaseStudyList()
+        default:
+            Text("Unknown case study '\(caseStudy.label)'")
         }
     }
 }
